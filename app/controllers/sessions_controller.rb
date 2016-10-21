75414fe1686267910271ccaed877d71f824ea488
forestf255 <forestf255@gmail.com>
2016-10-20 22:25:42 -0700

    finished login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      flash[:success] = 'Logged in.'
+      redirect_to home_path
+    else
+      flash.now[:failure] = 'Invalid email or password.'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out.'
+    redirect_to login_path
+  end
 end
