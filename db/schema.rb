5093fedba6496171f18dce854361be798d6931c2
forestf255 <forestf255@gmail.com>
2016-10-20 21:46:32 -0700

    half done login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019232709) do
+ActiveRecord::Schema.define(version: 20161021013045) do
+
   create_table "clients", force: :cascade do |t|
     t.string   "name"
     t.string   "counselor"
@@ -20,15 +21,13 @@ ActiveRecord::Schema.define(version: 20161019232709) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-end
 
-ActiveRecord::Schema.define(version: 20161019202904) do
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string	"seed"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
