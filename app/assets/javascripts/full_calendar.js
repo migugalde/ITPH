aacfe74cd05fe6281127ec0db8c6aa77b963f984
Hannah Tran <jolly.hannah@berkeley.edu>
2016-11-02 12:52:11 -0700

    Deleting works

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -3,7 +3,6 @@ initialize_calendar = function() {
   $('.calendar').each(function(){
     var calendar = $(this);
     calendar.fullCalendar({
-      defaultView: 'agendaWeek',
       header: {
         left: 'prev,next today',
         center: 'title',
@@ -16,7 +15,7 @@ initialize_calendar = function() {
       events: '/events.json',
 
       select: function(start, end) {
-        $.getScript('events/new.js', function() {
+        $.getScript('/events/new', function() {
           $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
           date_range_picker();
           $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
@@ -24,6 +23,30 @@ initialize_calendar = function() {
         });
 
         calendar.fullCalendar('unselect');
+      },
+
+      eventDrop: function(event, delta, revertFunc) {
+        event_data = { 
+          event: {
+            id: event.id,
+            start: event.start.format(),
+            end: event.end.format()
+          }
+        };
+        $.ajax({
+            url: event.update_url,
+            data: event_data,
+            type: 'PATCH'
+        });
+      },
+      
+      eventClick: function(event, jsEvent, view) {
+        $.getScript(event.edit_url, function() {
+          $('#event_date_range').val(moment(event.start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(event.end).format("MM/DD/YYYY HH:mm"))
+          date_range_picker();
+          $('.start_hidden').val(moment(event.start).format('YYYY-MM-DD HH:mm'));
+          $('.end_hidden').val(moment(event.end).format('YYYY-MM-DD HH:mm'));
+        });
       }
     });
   })
