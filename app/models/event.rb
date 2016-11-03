4c696cacc413319168b109a312cec6059bd292cc
Hannah Tran <jolly.hannah@berkeley.edu>
2016-11-03 16:00:09 -0700

    working something

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,18 @@
 class Event < ActiveRecord::Base
   validates :title, presence: true
+
   attr_accessor :date_range
 
   def all_day_event?
     self.start == self.start.midnight && self.end == self.end.midnight ? true : false
+  end
+
+  def self.modify_viewable_titles counselor
+    Event.where(:counselor => counselor).each do
+    	each.title = each.clients
+    end
+    Event.where.not(:counselor => counselor).each do
+	each.title = each.counselor
+    end
   end
 end
