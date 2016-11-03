839cd99fd7aacf7940089b99c850704b67ad175b
Matthew Smith <msmith1@berkeley.edu>
2016-11-03 18:41:43 +0000

    edited cucumber features

--- features/step_definitions/schedule_appt_steps.rb
+++ features/step_definitions/schedule_appt_steps.rb
@@ -1,36 +1,14 @@
-Given(/^I am logged in as Toni$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+
+Given(/^I am logged in as "([^"]*)"$/) do |arg|
+  # id = session[:user_id]
+  # user = User.find(:id == id)
+  @user = User.new
+  @user.email = arg + "@gmail.com"
 end
 
 Given(/^the following appointments exist:$/) do |table|
   # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/^I click "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I should see a message "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/^today is "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/^the time is "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I should see an error "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I should see "([^"]*)" is "([^"]*)"$/) do |arg1, arg2|
-  pending # Write code here that turns the phrase above into concrete actions
+  table.hashes.each do |event|
+    Event.create!(event)
+  end
 end
