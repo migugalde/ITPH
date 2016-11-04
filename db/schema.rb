0a9a3532f89df9aaea425591f80b969c1b76df7f
Tara Calista <tara.calista@berkeley.edu>
2016-11-04 05:42:19 +0000

    finished chores

--- db/schema.rb
+++ db/schema.rb
@@ -38,9 +38,12 @@ ActiveRecord::Schema.define(version: 20161101235359) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email"
     t.string   "password_hash"
     t.string   "password_salt"
+    t.string   "phone_number"
+    t.string   "admin"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
