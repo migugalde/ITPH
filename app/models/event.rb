c78bed94042b06e57abd99c136ef81521c5ef923
Hannah Tran <jolly.hannah@berkeley.edu>
2016-11-03 17:37:14 -0700

    added working tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,11 +8,14 @@ class Event < ActiveRecord::Base
     self.start == self.start.midnight && self.end == self.end.midnight ? true : false
   end
 
-  def self.user_events(user_id, e_start, e_end)
+  def self.user_events(user_id, e_start, e_end, counselor, client)
     events = Event.where(start: e_start..e_end)
     events.each do |event|
       if user_id == event.user.id then
         event.color = 'green'
+        event.title = client
+      else
+        event.title = counselor
       end
     end
     return events
