a94f54566f2dd8d4a13317d037057c0db5ef2ff8
Hannah Tran <jolly.hannah@berkeley.edu>
2016-11-15 12:26:05 -0800

    Updated daterangepicker to only match valid dates and default to one month views and other view changes ht

--- app/assets/javascripts/date_range_picker.js
+++ app/assets/javascripts/date_range_picker.js
@@ -3,8 +3,13 @@ date_range_picker = function() {
   $('.date-range-picker').each(function(){
     $(this).daterangepicker({
         timePicker: true,
-        timePickerIncrement: 30,
-        alwaysShowCalendars: true
+        timePickerIncrement: 10,
+        timePicker24Hour: false,
+        showDropdowns: true,
+        forceParse: true,
+        locale: {
+            format: 'MM/DD/YYYY h:mm A'
+        }
     }, function(start, end, label) {
       $('.start_hidden').val(start.format('YYYY-MM-DD HH:mm'));
       $('.end_hidden').val(end.format('YYYY-MM-DD HH:mm'));
