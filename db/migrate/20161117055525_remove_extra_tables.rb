6d9232409748fdc56e1556001a4991b5521935b6
Hannah Tran <jolly.hannah@berkeley.edu>
2016-11-17 12:05:41 -0800

    Trying to fix migration

--- db/migrate/20161117055525_remove_extra_tables.rb
+++
@@ -1,7 +0,0 @@
-class RemoveExtraTables < ActiveRecord::Migration
-  def change
-  	drop_table :fullcalendar_engine_event_series
-    	remove_column :events, :counselor, :string
-	remove_column :events, :clients, :string
-  end
-end
