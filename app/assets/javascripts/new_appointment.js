49591868e602f496e206d754f3eea12d58fe6123
abiagrawal <abiagrawal@berkeley.edu>
2016-11-17 12:59:35 -0800

    Javascript to add new modal

---
+++ app/assets/javascripts/new_appointment.js
@@ -0,0 +1,13 @@
+console.log('testing');
+
+$(document).ready(function() {
+    $('.appointment-button').click(function(e) {
+        e.preventDefault();
+        $.getScript('/events/new', function() {
+          $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
+          date_range_picker();
+          $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
+          $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
+        });
+    });
+})
