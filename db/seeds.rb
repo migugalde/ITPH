26868229ce45a06e1feb0012c95c8bc9a64194f5
Hannah Tran <jolly.hannah@berkeley.edu>
2017-01-19 16:18:55 -0800

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,15 @@
-6.times do
-	client = Client.new
- 	client.counselor = ['Toni', 'Bob'].sample
- 	client.name = Faker::Name.name
- 	client.email = "533petersave@gmail.com"
- 	client.phone_number = Faker::PhoneNumber.cell_phone
- 	client.save
-end
+users = [{:name => 'Toni', :email => 'toni@itph.org', :password => 'password', :password_confirmation => 'password', :admin => true}]
 
-users = [{:name => 'Toni', :email => 'toni@gmail.com', :password => 'password', :password_confirmation => 'password',:phone_number => '+2412412414', :admin => true},
-            {:name => 'Bob', :email => 'bob@gmail.com', :password => 'password', :password_confirmation => 'password', :phone_number => '+2412412414', :admin => false}]
-
-unless User.find_by( email: "toni@gmail.com" )
-  users.each do |user|
-    User.create!(user)
-  end
+users.each do |user|
+  User.create!(user)
 end
 
 
-rooms = [{:name => 'Room 1', :color => '#a4bdfc'},
-				{:name => 'Room 2', :color => '#ffb878'},
-				{:name => 'Room 3', :color => '#dbadff'}]
+rooms = [{:name => 'Tonis Room', :color => '#a4bdfc'},
+				{:name => 'Middle Room', :color => '#ffb878'},
+				{:name => 'First Room', :color => '#dbadff'},
+        {:name => 'Waiting Room', :color => '#800000'}]
 
 rooms.each do |room|
 	Room.create!(room)
-end
-
-40.times do
-  event = Event.new
-  event.title = Faker::Book.title
-  event.event_type = ["Meeting", "Group Event"].sample
-  event.room = Room.all.sample
-  event.notes = Faker::Hipster.paragraph
-	event.start = Faker::Time.between(7.days.ago, 14.days.from_now + 7.days, :all)
-  event.end = event.start + [1,2].sample.hours
-  event.date = event.start.to_date
-  event.save
-end
-
-Event.all.each do |event|
-  if (event.event_type == "Meeting")
-    event.users << [User.all.sample]
-    event.clients << [Client.all.sample]
-  else
-    event.users << [User.all.sample(rand(1..User.count))]
-    event.clients << [Client.all.sample(rand(1..Client.count))]
-  end
 end
